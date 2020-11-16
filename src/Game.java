e0c92da9888eb8546ca7455db9695564585a0bb9
VasilisPitsiavas <vpitsiavas83@gmail.com>
2020-11-17 00:29:03 +0200

    Added json manipulation

--- src/Game.java
+++ src/Game.java
@@ -1,19 +1,45 @@
-import java.util.Scanner;  
-import java.util.ArrayList;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser; 
+import java.io.FileReader;
+import java.util.Iterator;
 
+public class Game {     
+    private Player [] players;
 
-public class Game {
-    private ArrayList<String> questions;
-    private Player[] players;
-
-    public Game(int num) {
-        questions = new ArrayList<String>();
-        setGamesPlayers(num);
+    public Game() { 
     }
 
     public void setGamesPlayers(int num) {
         for (int i = 0; i < num; i++) {
-            this.players[i] = new Player(i + 1);
+            Player a = new Player(i);
         }
+    }
+
+
+
+    public void JsonParserr() {
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("Questions.json"));
+
+            JSONObject jsonObject = (JSONObject) obj;
+
+            JSONArray questionsList = (JSONArray) jsonObject.get("QuestionPool");
+
+            Iterator it = questionsList.iterator();
+            while(it.hasNext()) {
+                System.out.println(it.next());
+                
+            }
+    
+        }   catch (Exception e) {
+            e.printStackTrace();
+        }
+    } 
+    public static void main(String []args) {
+        Game g = new Game();
+        g.JsonParserr();
+        
     }
 }
