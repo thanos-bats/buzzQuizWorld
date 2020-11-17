ca75a137340892ccbc20e90fb31ca52f46ceff32
VasilisPitsiavas <vpitsiavas83@gmail.com>
2020-11-17 23:43:07 +0200

    Iniial commit for Round class

---
+++ src/Round.java
@@ -0,0 +1,9 @@
+import java.util.Iterator;
+
+public class Round {
+    
+    public Round() {
+        QuestionPool q = new QuestionPool();
+        System.out.println("New Round has began");
+    }
+}
