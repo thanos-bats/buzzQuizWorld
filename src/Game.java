e51558d73afc7fbbe447b6459e000496ec614f1c
mpatsiou <batsioularas@gmail.com>
2020-11-18 19:49:21 +0200

    Create the constructor of Game and related functions

--- src/Game.java
+++ src/Game.java
@@ -1,45 +1,27 @@
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser; 
-import java.io.FileReader;
-import java.util.Iterator;
 
 public class Game {     
-    private Player [] players;
+    private Player[] players;
+    private Round[] rounds;
+    private QuestionPool pool;
 
-    public Game() { 
+
+    public Game(int numberOfPlayers,int numberOfRounds, int numberOfQuestions) {
+        pool = new QuestionPool();
+
+        players = new Player[numberOfPlayers];
+        this.addPlayers(numberOfPlayers);
+
+        rounds = new Round[numberOfQuestions];
+
     }
 
-    public void setGamesPlayers(int num) {
+    public void addPlayers(int num) {
         for (int i = 0; i < num; i++) {
-            Player a = new Player(i);
+            this.players[i] = new Player(i);
         }
     }
 
-
-
-    public void JsonParserr() {
-        JSONParser parser = new JSONParser();
-        try {
-            Object obj = parser.parse(new FileReader("Questions.json"));
-
-            JSONObject jsonObject = (JSONObject) obj;
-
-            JSONArray questionsList = (JSONArray) jsonObject.get("QuestionPool");
-
-            Iterator it = questionsList.iterator();
-            while(it.hasNext()) {
-                System.out.println(it.next());
-                
-            }
-    
-        }   catch (Exception e) {
-            e.printStackTrace();
-        }
-    } 
-    public static void main(String []args) {
-        Game g = new Game();
-        g.JsonParserr();
-        
+    public void initRound(int numRound) {
+        this.rounds[numRound].initQuestions(this.pool.getXQuestions(this.rounds.length));
     }
 }
