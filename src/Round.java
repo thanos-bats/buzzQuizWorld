9621185072b851f20f592f81aec1a6d61ac5b69a
mpatsiou <batsioularas@gmail.com>
2020-11-18 22:34:04 +0200

    Change the Round constructor

--- src/Round.java
+++ src/Round.java
@@ -3,11 +3,7 @@ import java.util.ArrayList;
 public class Round {
     private Question[] roundQuestions;
 
-    public Round(int numberOfQuestions, QuestionPool pool ) {
-        this.roundQuestions = new Question[numberOfQuestions];
-    }
-
-    public void initQuestions(Question[] q) {
+    public Round(Question[] q) {
         roundQuestions = q;
     }
 
