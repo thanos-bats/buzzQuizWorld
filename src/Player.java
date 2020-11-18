78813cf3927d7d4af80c695c63b06c69fe72da42
mpatsiou <batsioularas@gmail.com>
2020-11-18 22:32:01 +0200

    Read answer form player and update score

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Player {
-    private int id;
+    private final int id;
+    private double score;
 
     public Player(int id) {
         this.id = id;
+        this.score = 0.0;
     }
+
+    public int readAnswer() {
+        Scanner scanner = new Scanner(System.in);
+        int answer = scanner.nextInt() - 1;
+
+        return answer;
+    }
+
+    public void updateScore(double points) { this.score += points; }
+
 }
