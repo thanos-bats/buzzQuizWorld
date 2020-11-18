aaa680412cead55b30624d06ca69529afac55671
mpatsiou <batsioularas@gmail.com>
2020-11-18 19:54:06 +0200

    Deleted the previous compiler

--- src/Player.java
+++ src/Player.java
@@ -1,15 +1,7 @@
-
-
 public class Player {
     private int id;
-    private int CorrectAnswers;
-    private int HighScore;
-    /*for multiplayer*/
-    private int wins;
 
-    
     public Player(int id) {
         this.id = id;
-        this.CorrectAnswers = 0;
     }
 }
