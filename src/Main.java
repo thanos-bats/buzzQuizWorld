ca71c1b27f8bd7e87904fc8246d2feda37883b13
mpatsiou <batsioularas@gmail.com>
2020-12-01 23:32:42 +0200

    Add play function on Game

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Cli ui = new Cli();
         Game g = new Game(ui);
 
