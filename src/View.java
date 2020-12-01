6e309689ca3b3fa86828bf7f4d02fce417db3afe
mpatsiou <batsioularas@gmail.com>
2020-12-01 23:30:08 +0200

    Minor changes

--- src/View.java
+++ src/View.java
@@ -1,11 +1,6 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.ArrayList;
-
 public interface View {
 
     //public int getNumOfPlayers();
     public int readAnswer();
     public void showQuestion(Question q);
-
 }
