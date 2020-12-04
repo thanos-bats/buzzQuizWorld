99626a2df241a8a7f5148e42e48e8fa50b15b1d2
VasilisPitsiavas <vpitsiavas83@gmail.com>
2020-12-04 18:22:08 +0200

    Changed runRound

--- src/ClassicRound.java
+++ src/ClassicRound.java
@@ -1,21 +1,21 @@
 public class ClassicRound extends Round {
-    //Mabe useless 
 
     private long score =  0;
     
     public ClassicRound(Question[] q, View ui) {
         super(q, ui);
-        checkIfIsRight();
+        runRound();
     }
 
-    public void checkIfIsRight() {
+    public void runRound() {
         for (Question q : roundQuestions) {
             ui.showQuestion(q);
-            if (q.checkRightAnswer(q.answers[ui.readAnswer() + 1])) {
+            if (q.checkRightAnswer(q.answers[ui.readAnswer() - 1])) {
                 System.out.println("Correct, You earned 1000 points"); //Mabe it will be to the interface
                 score +=1000.0;
+            } 
+            else { System.out.println("You answer is wrong!!!"); //Mabe it will be to the interface
             }
-            System.out.println("You answer is wrong!!!"); //Mabe it will be to the interface
         }      
     }
 }
