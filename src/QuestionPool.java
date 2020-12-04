a01c3366f9889f58262ec5fca02822ef7cbe24c6
VasilisPitsiavas <vpitsiavas83@gmail.com>
2020-12-04 20:03:25 +0200

    Added javadoc

--- src/QuestionPool.java
+++ src/QuestionPool.java
@@ -1,3 +1,8 @@
+/* 
+  *Class QuestionPool imprements a pool of Questions
+  *QuestionPool includes an Array of Question and inits it with the questions
+*/
+
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -8,11 +13,13 @@ import java.util.Random;
 public class QuestionPool {
     private ArrayList<Question> pool;
 
+/* Constructor of the Class */
     public QuestionPool() throws IOException {
         pool = new ArrayList<>();
         this.initPool();
     }
 
+/*  InitPool is a function which fills the Question Array with questions from the external txt file*/
     public void initPool() throws IOException {
         Path currentRelativePath = Paths.get("QuestionsFile.txt");
         File file = new File(currentRelativePath.toAbsolutePath().toString());
@@ -24,12 +31,14 @@ public class QuestionPool {
             String[] line = str.split(",");
             pool.add(new Question(line[0], line[5], new String[] {line[1], line[2], line[3], line[4]}, line[6]));
         }
+    
     }
-
+/*  @return the Arraylist of Question*/
     public ArrayList<Question> getPool() {
         return this.pool;
     }
 
+/*  @return random Questions from the pool */
     public Question getRandomQuestion() {
         Question randomQ;
         Random rn = new Random();
@@ -38,7 +47,7 @@ public class QuestionPool {
         this.deleteQuestion(randint);
         return randomQ;
     }
-
+/*  @return an Question array with X Questions */
     public Question[] getXQuestions(int num) {
         Question[] q = new Question[num];
         for (int i = 0; i < num; i++) {
@@ -46,7 +55,7 @@ public class QuestionPool {
         }
         return  q;
     }
-
+/* Deletes a Question from the pool */
     public void deleteQuestion(int i) {
         this.pool.remove(i);
     }
