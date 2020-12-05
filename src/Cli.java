3895254547c5b50f6ea50549d54bfb5c8ac160cf
mpatsiou <batsioularas@gmail.com>
2020-12-05 22:38:04 +0200

    Complete all the functions of interface View

--- src/Cli.java
+++ src/Cli.java
@@ -1,8 +1,11 @@
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 public class Cli implements View {
     public Cli() {
-        System.out.println("Hello, let's play Buzz!");
         //getNumOfPlayers();
     }
 
@@ -19,33 +22,95 @@ public class Cli implements View {
     } */
 
     @Override
-    public int readAnswer() {
+    public void welcome(){
+        System.out.println("Welcome");
+        System.out.println("Hello, let's play Buzz!\n\n");
+    }
+
+    @Override
+    public void betDetails() throws InterruptedException {
+        System.out.println("Let's bet some points now.");
+        System.out.println("You can bet 250, 500, 750 or 1000 points and if you answer correclty, you get back the doubles");
+        this.sleep(1);
+        System.out.println("GOOD LUCK\n");
+        this.sleep (3);
+    }
+
+    @Override
+    public void classicDetails() throws InterruptedException {
+        System.out.println("Let's play Classic Round.");
+        System.out.println ("For every correct answer you earn 1000 points.");
+        this.sleep(1);
+        System.out.println("GOOD LUCK\n");
+        this.sleep(3);
+    }
+
+    @Override
+    public void worgAnswerMessage(double points) throws InterruptedException {
+        System.out.println("Your answer is worg");
+        if (points != 0) {
+            System.out.println("You lost " + points + " points\n");
+        }
+        this.sleep(2);
+    }
+
+    @Override
+    public void correctAnswerMessage(double points) throws InterruptedException {
+        System.out.println("Correct. You earned " + points + " points\n");
+        this.sleep(2);
+    }
+
+    @Override
+    public void showBetValues(int[] values) {
+        StringBuilder str = new StringBuilder ("How many points do you want to bet?");
+        for (int v : values) {
+            str.append(" ").append(v).append(",");
+        }
+        str = new StringBuilder (str.substring (0, str.length () - 1));
+        System.out.println(str);
+    }
+
+    @Override
+    public void showScore(Player[] players) {
+        for (Player p : players) {
+            System.out.println("The " + (p.getId () + 1) + "st player has " + p.getScore () + " points\n");
+        }
+    }
+
+    @Override
+    public int readAnswer(Integer[] values) {
+        Set<Integer> set = new HashSet<>(Arrays.asList(values));
+
         Scanner sc = new Scanner(System.in);
         int an;
         do {
-            System.out.println("Write your answer here -> ");
+            System.out.println("Write your answer");
             an = sc.nextInt();
-            if (an < 1 || an > 4) {
-                System.out.println("Please write 1,2,3 or 4");
+            if (!set.contains(an)) {
+                System.out.println("Please, give correct value.");
             }
-        } while (an != 1 && an != 2 && an != 3 && an != 4);
+        } while (!set.contains(an));
 
         return an;
     }
 
     @Override
-    public void showQuestion(Question q) {
-        System.out.println(q.getCategory());
+    public void showQuestion(Question q) throws InterruptedException {
+        System.out.println("The category of answer is: " + q.getCategory());
+        this.sleep(2);
         System.out.println(q.getQuestion());
+        this.sleep(4);
         int i = 1;
-        for (String an : q.getAnswers()) {
+        for (String an : q.suffleAnswers()) {
             System.out.println(i + " " + an);
+            this.sleep(1);
             i++;
         }
     }
 
     @Override
-    public void showMessage(String message) {
-        System.out.println(message);
+    public void sleep(int i) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(i);
     }
+
 }
--- src/View.java
+++ src/View.java
@@ -1,7 +1,15 @@
 public interface View {
 
     //public int getNumOfPlayers();
-    int readAnswer();
-    void showQuestion(Question q);
-    void showMessage(String message);
+    void welcome();
+    void betDetails() throws InterruptedException;
+    void classicDetails() throws InterruptedException;
+
+    void correctAnswerMessage(double points) throws InterruptedException;
+    void worgAnswerMessage(double points) throws InterruptedException;
+    void showBetValues(int[] values);
+    void showScore(Player[] players);
+    int readAnswer(Integer[] values);
+    void showQuestion(Question q) throws InterruptedException;
+    void sleep(int i) throws InterruptedException;
 }
