e3a98e9acd031f7e4a64ddc3b454c83fa17a4096
mpatsiou <batsioularas@gmail.com>
2020-12-05 22:41:55 +0200

    Add InterruptedException

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Cli ui = new Cli();
         Game g = new Game(ui);
     }
