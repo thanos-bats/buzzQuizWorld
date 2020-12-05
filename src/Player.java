5fcebe6f79e885b60dfc96961b75914d4c3ed472
mpatsiou <batsioularas@gmail.com>
2020-12-05 22:40:38 +0200

    GetId function

--- src/Player.java
+++ src/Player.java
@@ -12,4 +12,5 @@ public class Player {
     public void updateScore(double points) { this.score += points; }
     
     public double getScore() { return this.score; }
+    public int getId() { return this.id; }
 }
